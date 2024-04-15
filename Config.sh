287f2fa69c07416ce1810802b1f79a4e42e957fd
Yusif <87138353+yusifibrahimov1@users.noreply.github.com>
2024-04-16 00:26:14 +0400

    Create Config.sh

---
+++ Config.sh
@@ -0,0 +1,17 @@
+#------------------------------------------------------------------------------
+# STATISTICS
+#------------------------------------------------------------------------------
+
+# track_activities = on
+# track_activity_query_size = 1024
+# track_counts = on
+track_io_timing = on
+track_wal_io_timing = on
+# track_functions = none
+# stats_fetch_consistency = cache
+
+compute_query_id = on
+# log_statement_stats = off
+# log_parser_stats = off
+# log_planner_stats = off
+# log_executor_stats = off
